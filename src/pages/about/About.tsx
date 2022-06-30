1ebcb49e7f91c65b19a1fa2b484f29a4020b3b57
Daniel Duarte <danielduarte.tech@protonmail.com>
2022-06-30 16:43:27 -0300

    Portfoio-Dev

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,20 @@
+export function About() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>Front-end</h2>
+                    <p>in a modern and responsivy way</p>
+                    
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <h1>I'm Daniel Duarte</h1>
+                <p>Nothing is better than program with only keybord's sound.</p>
+                <p>My journey as a full-stack developer beginned nearly 6 years ago when I was starting my engineering computer course. I'm fascinated by AIs/Machine Learning so it is my hobbie. But my passion is codding web pages and integrating with back-end.</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/menu/Menu.tsx
@@ -0,0 +1,13 @@
+import orbital from '../../assets/svg/orbital_icons.svg'
+import './index.scss'
+
+export function Menu() {
+    return(
+        <div>
+            <img id="orbital" src={orbital} />
+            <p>
+                <a className='text-regular text-regular-pink'>const </a><a className='text-bold text-bold-purple'>DanielDuarte</a><a className='text-regular text-regular-pink'>:</a><a className='text-bold text-bold-cian'> Developer </a><a className='text-regular text-regular-pink'>=</a> <a className='text-regular'>&#123;</a><br /><a className='text-regular'>&emsp;type:</a><a className='text-bold'> "Full-Stack"</a><a className='text-regular'>,</a><br /><a className='text-regular'>&emsp;language:</a><a className='text-bold'> "Typescript"</a><br /><a className='text-regular'>&#125;</a>
+            </p>
+      </div>
+    )
+}
