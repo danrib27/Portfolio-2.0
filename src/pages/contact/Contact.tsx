9526f71a36718a5a742ddb1f66e66ab8d98677fd
Daniel Duarte <danielduarte.tech@protonmail.com>
2022-07-18 21:07:11 -0300

    feat: add Telegram copy

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -14,6 +14,7 @@ export function Contact() {
 
     const  [copyButton01, setCopyButton01] = useState(false);
     const  [copyButton02, setCopyButton02] = useState(false);
+    const  [copyButton03, setCopyButton03] = useState(false);
 
     function validation(e:React.FormEvent<HTMLFormElement>) {
         let empty = false;
@@ -63,6 +64,13 @@ export function Contact() {
         }, 1400);
         return ()=> clearInterval(time);
     }
+    function handleButton3() {
+        setCopyButton03(true);
+        const time = setTimeout(()=>{
+            setCopyButton03(false);
+        }, 1400);
+        return ()=> clearInterval(time);
+    }
 
     return (
         <div className="contact">
@@ -77,6 +85,7 @@ export function Contact() {
                     <button type="submit">Send</button>
                 </form>
             </div>
+
             <div id="map">
                 <div className='map-container'>
                     <div className='address-container'>
@@ -98,76 +107,109 @@ export function Contact() {
                         </a>
                     </div>
                     <div className='google-map'>
-                    <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16318114.47700193!2d-57.109252977854815!3d-3.1172745325950837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92a46669f5986e5f%3A0xe336db2d6ab189cc!2sBel%C3%A9m%20-%20PA!5e0!3m2!1spt-BR!2sbr!4v1657311961850!5m2!1spt-BR!2sbr" width="320" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-
-                <div className='copy-container'>
-                <span className='email-copy'>danielduarte.tech@protonmail.com</span>
-                <button className='copy-button' onClick={()=>{
-                    copy('danielduarte.tech@protonmail.com');
-                    handleButton1();
-                }}>
-                {copyButton01
-                    ? <div className='copied-container'>
-                        <svg viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
-                            <circle fill="#4CAF50" cx="24" cy="24" r="21"/>
-                            <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
-                        </svg>
-                        <span className='copied-span'>Copied !</span>
-                    </div>    
-                    : <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460 460" xmlSpace="preserve">
-                        <g>
-                        	<g>
-                        		<g>
-                        			<path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272
-                        				c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729
-                        				c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"/>
-                        			<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272
-                        				c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068
-                        				c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001
-                        				C291.206,428.715,289.92,430,288.341,430z"/>
-                        		</g>
-                        	</g>
-                        </g>
-                    </svg>
-                }
-                </button>
-                </div>
-
-                <div className='copy-container'>
-                <span className='website-copy'>danielduarte.dev</span>
-                <button className='copy-button' onClick={()=>{
-                    copy('https://danielduarte.dev');
-                    handleButton2();
-                }}>
-                {copyButton02
-                    ? <div className='copied-container'>
-                        <svg viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
-                            <circle fill="#4CAF50" cx="24" cy="24" r="21"/>
-                            <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
-                        </svg>
-                        <span className='copied-span'>Copied !</span>
+                        <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16318114.47700193!2d-57.109252977854815!3d-3.1172745325950837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x92a46669f5986e5f%3A0xe336db2d6ab189cc!2sBel%C3%A9m%20-%20PA!5e0!3m2!1spt-BR!2sbr!4v1657311961850!5m2!1spt-BR!2sbr" width="320" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
-                    : <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460 460" xmlSpace="preserve">
-                        <g>
-                        	<g>
-                        		<g>
-                        			<path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272
-                        				c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729
-                        				c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"/>
-                        			<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272
-                        				c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068
-                        				c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001
-                        				C291.206,428.715,289.92,430,288.341,430z"/>
-                        		</g>
-                        	</g>
-                        </g>
-                    </svg>
-                }
-                </button>
-                </div>
 
-                <button className='cv-button'>Download CV</button>
+                    <div className='copy-container'>
+                        <span className='email-copy'>danielduarte.tech@protonmail.com</span>
+                        <button className='copy-button' onClick={()=>{
+                            copy('danielduarte.tech@protonmail.com');
+                            handleButton1();
+                        }}>
+                        {copyButton01
+                            ? <div className='copied-container'>
+                                <svg viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
+                                    <circle fill="#4CAF50" cx="24" cy="24" r="21"/>
+                                    <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
+                                </svg>
+                                <span className='copied-span'>Copied !</span>
+                            </div>    
+                            : <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460 460" xmlSpace="preserve">
+                                <g>
+                                	<g>
+                                		<g>
+                                			<path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272
+                                				c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729
+                                				c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"/>
+                                			<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272
+                                				c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068
+                                				c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001
+                                				C291.206,428.715,289.92,430,288.341,430z"/>
+                                		</g>
+                                	</g>
+                                </g>
+                            </svg>
+                        }
+                        </button>
+                    </div>
+
+                    <div className='copy-container'>
+                        <span className='website-copy'>danielduarte.dev</span>
+                        <button className='copy-button' onClick={()=>{
+                            copy('https://danielduarte.dev');
+                            handleButton2();
+                        }}>
+                        {copyButton02
+                            ? <div className='copied-container'>
+                                <svg viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
+                                    <circle fill="#4CAF50" cx="24" cy="24" r="21"/>
+                                    <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
+                                </svg>
+                                <span className='copied-span'>Copied !</span>
+                            </div>
+                            : <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460 460" xmlSpace="preserve">
+                                <g>
+                                	<g>
+                                		<g>
+                                			<path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272
+                                				c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729
+                                				c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"/>
+                                			<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272
+                                				c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068
+                                				c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001
+                                				C291.206,428.715,289.92,430,288.341,430z"/>
+                                		</g>
+                                	</g>
+                                </g>
+                            </svg>
+                        }
+                        </button>
+                    </div>
+
+                    <div className='copy-container'>
+                        <span className='website-copy'>@danrib27</span>
+                        <button className='copy-button' onClick={()=>{
+                            copy('@danrib27');
+                            handleButton3();
+                        }}>
+                        {copyButton03
+                            ? <div className='copied-container'>
+                                <svg viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
+                                    <circle fill="#4CAF50" cx="24" cy="24" r="21"/>
+                                    <polygon fill="#CCFF90" points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"/>
+                                </svg>
+                                <span className='copied-span'>Copied !</span>
+                            </div>
+                            : <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460 460" xmlSpace="preserve">
+                                <g>
+                                	<g>
+                                		<g>
+                                			<path d="M425.934,0H171.662c-18.122,0-32.864,14.743-32.864,32.864v77.134h30V32.864c0-1.579,1.285-2.864,2.864-2.864h254.272
+                                				c1.579,0,2.864,1.285,2.864,2.864v254.272c0,1.58-1.285,2.865-2.864,2.865h-74.729v30h74.729
+                                				c18.121,0,32.864-14.743,32.864-32.865V32.864C458.797,14.743,444.055,0,425.934,0z"/>
+                                			<path d="M288.339,139.998H34.068c-18.122,0-32.865,14.743-32.865,32.865v254.272C1.204,445.257,15.946,460,34.068,460h254.272
+                                				c18.122,0,32.865-14.743,32.865-32.864V172.863C321.206,154.741,306.461,139.998,288.339,139.998z M288.341,430H34.068
+                                				c-1.58,0-2.865-1.285-2.865-2.864V172.863c0-1.58,1.285-2.865,2.865-2.865h254.272c1.58,0,2.865,1.285,2.865,2.865v254.273h0.001
+                                				C291.206,428.715,289.92,430,288.341,430z"/>
+                                		</g>
+                                	</g>
+                                </g>
+                            </svg>
+                        }
+                        </button>
+                    </div>
+                    
+                    <span className='number-text'>If you wanna talk to me directly, this is my Telegram. </span>
                 </div>
             </div>
         </div>
